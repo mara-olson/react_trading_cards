052645d8c7b6f62ecca410bc62a717e5a8521df0
mara-olson <mara.c.olson@gmail.com>
2022-04-02 17:47:31 -0700

    Mount and render components on homepage and /cards

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,7 +1,14 @@
-'use strict';
+"use strict";
 
 function Homepage() {
-  return <div>This component needs to be implemented.</div>;
+  return (
+    <div>
+      <h1>Welcome once more.</h1>
+      <a href="/cards">Check out these cards!</a>
+      <br></br>
+      <img src="/static/img/balloonicorn.jpg" />
+    </div>
+  );
 }
 
-ReactDOM.render(<Homepage />, document.querySelector('#app'));
+ReactDOM.render(<Homepage />, document.querySelector("#app"));
--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
 const tradingCardData = [
   {
-    name: 'Balloonicorn',
-    skill: 'video games',
-    imgUrl: '/static/img/balloonicorn.jpg',
+    name: "Balloonicorn",
+    skill: "video games",
+    imgUrl: "/static/img/balloonicorn.jpg",
   },
 ];
 
@@ -13,22 +13,34 @@ function TradingCard(props) {
     <div className="card">
       <h2>Name: {props.name}</h2>
       <img src={props.imgUrl} alt="profile" />
-      <h2>Skill: </h2>
+      <h2>Skill: {props.skill}</h2>
     </div>
   );
 }
 
 ReactDOM.render(
-  <TradingCard name="Balloonicorn" skill="video games" imgUrl="/static/img/balloonicorn.jpg" />,
-  document.querySelector('#balloonicorn'),
+  <TradingCard
+    name="Balloonicorn"
+    skill="video games"
+    imgUrl="/static/img/balloonicorn.jpg"
+  />,
+  document.querySelector("#balloonicorn")
 );
 
 ReactDOM.render(
-  <TradingCard name="Float" skill="baking pretzels" imgUrl="/static/img/float.jpg" />,
-  document.querySelector('#float'),
+  <TradingCard
+    name="Float"
+    skill="baking pretzels"
+    imgUrl="/static/img/float.jpg"
+  />,
+  document.querySelector("#float")
 );
 
 ReactDOM.render(
-  <TradingCard name="Llambda" skill="knitting scarves" imgUrl="/static/img/llambda.jpg" />,
-  document.querySelector('#llambda'),
+  <TradingCard
+    name="Llambda"
+    skill="knitting scarves"
+    imgUrl="/static/img/llambda.jpg"
+  />,
+  document.querySelector("#llambda")
 );
